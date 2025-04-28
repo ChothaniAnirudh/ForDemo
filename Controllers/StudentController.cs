764be58830d378431fcef5e22d919feafb4456f5
TeamOpine Solutions <code@teamopine.com>
2025-04-28 15:43:16 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using ForDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<StudentModel> GetStudents()
+        {
+            return CollageRepository.Students;
+
+        }
+    }
+}
---
+++ Models/CollageRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ForDemo.Models
+{
+    public static class CollageRepository
+    {
+        public static IEnumerable<StudentModel> Students { get; set; } = 
+            new List<StudentModel>() {
+            new StudentModel{ Id=1, Name="Ani", Email="chothani@gmail.com", Address="ok" },
+            new StudentModel{ Id=2, Name="rj", Email="jn@gmail.com", Address="done"}
+
+            };
+    }
+}
